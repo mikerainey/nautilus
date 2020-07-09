788b6d13eadbed43d689846b6c2c876e8bad0e28
mikerainey <me@mike-rainey.site>
2020-07-08 21:56:07 -0400

    adding fib benchmark

--- src/rt/tpal/bench.cc
+++ src/rt/tpal/bench.cc
@@ -1,6 +1,7 @@
 #include "incr_array.hpp"
 #include "plus_reduce_array.hpp"
 #include "spmv.hpp"
+#include "fib.hpp"
 
 #include "tpalrts.hpp"
 
@@ -96,7 +97,7 @@ extern "C" {
 /* Plus reduce array */
 
 namespace tpalrts {
-namespace plus_array_reduce {
+namespace plus_reduce_array {
 
 uint64_t nb_items = 1000 * 1000 * 1000;
 int64_t* a;
@@ -165,16 +166,16 @@ void bench_plus_reduce_array_manual() {
 
 extern "C" {
   void handle_plus_reduce_array_interrupt(char *buf, void *priv) {
-    tpalrts::plus_array_reduce::bench_plus_reduce_array_interrupt();
+    tpalrts::plus_reduce_array::bench_plus_reduce_array_interrupt();
   }
   void handle_plus_reduce_array_software_polling(char *buf, void *priv) {
-    tpalrts::plus_array_reduce::bench_plus_reduce_array_software_polling();
+    tpalrts::plus_reduce_array::bench_plus_reduce_array_software_polling();
   }
   void handle_plus_reduce_array_manual(char *buf, void *priv) {
-    tpalrts::plus_array_reduce::bench_plus_reduce_array_manual();
+    tpalrts::plus_reduce_array::bench_plus_reduce_array_manual();
   }
   void handle_plus_reduce_array_serial(char *buf, void *priv) {
-    tpalrts::plus_array_reduce::bench_plus_reduce_array_serial();
+    tpalrts::plus_reduce_array::bench_plus_reduce_array_serial();
   }
 }
 
@@ -352,6 +353,87 @@ extern "C" {
   }
   void handle_spmv_serial(char *buf, void *priv) {
     tpalrts::bench_spmv::bench_spmv_serial();
+  }
+}
+
+/*---------------------------------------------------------------------*/
+/* Fib */
+
+namespace tpalrts {
+namespace fib {
+
+uint64_t n = 43;
+uint64_t r = 0;
+tpalrts::stack_type s;
+  
+auto bench_pre() -> void {
+  
+}
+
+auto bench_body_interrupt(promotable* p) -> void {
+  rollforward_table = {
+  };
+  s = tpalrts::snew();
+  fib_heartbeat<heartbeat_mechanism_hardware_interrupt>(n, &r, p, 128, s, fib_heartbeat_entry, 0);  
+}
+
+auto bench_body_software_polling(promotable* p) -> void {
+  s = tpalrts::snew();
+  fib_heartbeat<heartbeat_mechanism_software_polling>(n, &r, p, 128, s, fib_heartbeat_entry, 0);
+}
+
+auto bench_body_serial(promotable* p) -> void {
+  s = tpalrts::snew();
+  r = fib_custom_stack_serial(n, 128, s);
+}
+
+auto bench_post() -> void {
+  if (s.stack != nullptr) {
+    sdelete(s);
+  }
+  assert(r == fib_serial(n));
+}
+
+void bench_fib_array_interrupt() {
+  using microbench_scheduler_type = mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, ping_thread_worker, ping_thread_interrupt>;
+  launch<microbench_scheduler_type, ping_thread_worker, ping_thread_interrupt>(nb_workers, bench_pre, bench_post, bench_body_interrupt);
+}
+
+void bench_fib_array_software_polling() {
+  using microbench_scheduler_type = mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, tpal_worker>;
+  launch<microbench_scheduler_type, tpal_worker, mcsl::minimal_interrupt>(nb_workers, bench_pre, bench_post, bench_body_software_polling);
+}
+
+void bench_fib_array_serial() {
+  using microbench_scheduler_type = mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, tpal_worker>;
+  launch<microbench_scheduler_type, tpal_worker, mcsl::minimal_interrupt>(nb_workers, bench_pre, bench_post, bench_body_serial);
+}
+
+void bench_fib_array_manual() {
+  mcsl::perworker::unique_id::initialize(nb_workers);
+  using microbench_scheduler_type = mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, tpal_worker>;
+  bench_pre();
+  auto bench_body_manual = new fib_manual<microbench_scheduler_type>(n, &r);
+  auto bench_pre2 = [] { };
+  launch0<microbench_scheduler_type, tpal_worker, mcsl::minimal_interrupt>(nb_workers, bench_pre2, bench_post, bench_body_manual);
+}
+
+  
+} // end namespace
+} // end namespace
+
+extern "C" {
+  void handle_fib_array_interrupt(char *buf, void *priv) {
+    tpalrts::fib::bench_fib_array_interrupt();
+  }
+  void handle_fib_array_software_polling(char *buf, void *priv) {
+    tpalrts::fib::bench_fib_array_software_polling();
+  }
+  void handle_fib_array_manual(char *buf, void *priv) {
+    tpalrts::fib::bench_fib_array_manual();
+  }
+  void handle_fib_array_serial(char *buf, void *priv) {
+    tpalrts::fib::bench_fib_array_serial();
   }
 }
 
