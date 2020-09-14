c46b3366f83f15fa08d294207eecb65755682572
mikerainey <bionic.oldsmobile@gmail.com>
2020-09-14 16:56:43 -0400

    improve shell.nix

--- scripts/tpal.sh
+++ scripts/tpal.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Usage:
+#  tpal.sh <path to tpal> <path to mcsl> <path to nautilus>
 
 TPAL_PATH=$1
 TPAL_RUNTIME_PATH="${TPAL_PATH}/runtime/"
@@ -6,24 +8,35 @@ TPAL_RUNTIME_HEADER_FILES="${TPAL_RUNTIME_PATH}/include/*.hpp"
 TPAL_RUNTIME_BENCH_HEADER_FILES="${TPAL_RUNTIME_PATH}/bench/*.hpp"
 TPAL_RUNTIME_BENCH_ASM_FILES="${TPAL_RUNTIME_PATH}/bench/*_manual.s"
 
-NAUTILUS_PATH=$2
+MCSL_PATH=$2
+MCSL_INCLUDE_PATH="$MCSL_PATH/include/"
+MCSL_HEADER_FILES="$MCSL_INCLUDE_PATH/*.hpp"
+
+NAUTILUS_PATH=$3
 NAUTILUS_TPAL_INCLUDE_PATH="${NAUTILUS_PATH}/include/rt/tpal/"
 NAUTILUS_TPAL_BENCH_PATH="${NAUTILUS_PATH}/src/rt/tpal/"
+NAUTILUS_MCSL_INCLUDE_PATH="${NAUTILUS_PATH}/include/rt/mcsl/"
 
 # Set symlinks for tpal header files
 for f in $TPAL_RUNTIME_HEADER_FILES
 do
-    ln -s $f ${NAUTILUS_TPAL_INCLUDE_PATH}/$(basename $f)
+    ln -sf $f ${NAUTILUS_TPAL_INCLUDE_PATH}/$(basename $f)
 done
 
 # Set symlinks for tpal benchmark header files
 for f in $TPAL_RUNTIME_BENCH_HEADER_FILES
 do
-    ln -s $f ${NAUTILUS_TPAL_BENCH_PATH}/$(basename $f)
+    ln -sf $f ${NAUTILUS_TPAL_BENCH_PATH}/$(basename $f)
 done
 
 # Set symlinks for tpal benchmark assembly files
 for f in $TPAL_RUNTIME_BENCH_ASM_FILES
 do
-    ln -s $f ${NAUTILUS_TPAL_BENCH_PATH}/$(basename $f)
+    ln -sf $f ${NAUTILUS_TPAL_BENCH_PATH}/$(basename $f)
+done
+
+# Set symlinks for mcsl header files
+for f in $MCSL_HEADER_FILES
+do
+    ln -sf $f ${NAUTILUS_MCSL_INCLUDE_PATH}/$(basename $f)
 done
--- shell.nix
+++ shell.nix
@@ -1,24 +1,43 @@
 # Usage:
 #   $ nix-shell
-#   ... nautilus build ...
+#
+# To run nautilus via default.nix:
 #   $ nautilus
-#   ... boot nautilus in qemu ...
+#   ... boots nautilus in qemu ...
+#
+# To run nautilus locally:
+#   $ nautilus_link_tpal
+#   ... sets up sym links, assuming tpal and mcsl are located in ../ ...
+#  $ nautilus-local
+#  ... boots nautilus in qemu ...
 
 { pkgs   ? import <nixpkgs> {},
   stdenv ? pkgs.stdenv,
-  nautilusSrc ? ./.,
+  gcc ? pkgs.gcc,
+  grub ? pkgs.grub2,
+  binutils ? pkgs.binutils,
+  xorriso ? pkgs.xorriso,
+  sources ? import ./local-sources.nix,
+  mcsl ? sources.mcsl,
+  tpalSrc ? sources.tpalSrc,
+  nautilusSrc ? sources.nautilusSrc,
   nautilusConfig ? "${nautilusSrc}/configs/default-config",
   nautilus ? import ./default.nix { pkgs = pkgs;
-                                    nautilusSrc = nautilusSrc;
+                                    sources = sources;
                                     nautilusConfig = nautilusConfig;
+                                    grub = grub;
+                                    binutils = binutils;
+                                    xorriso = xorriso;
                                   },
   qemu ? pkgs.qemu
 }:
   
 stdenv.mkDerivation {
   name = "nautilus-shell";
-  buildInputs = [ qemu nautilus ];
+  buildInputs = [ qemu nautilus gcc grub xorriso binutils ];
   shellHook = ''
     alias nautilus="qemu-system-x86_64 -cdrom ${nautilus}/nautilus.iso -m 2048 -curses -nographic -smp 4"
+    alias nautilus_link_tpal="./scripts/tpal.sh `pwd`/../tpal `pwd`/../mcsl `pwd`"
+    alias nautilus-local="qemu-system-x86_64 -cdrom ./nautilus.iso -m 2048 -curses -nographic -smp 4"
   '';
 }
