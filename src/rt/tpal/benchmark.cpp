f175426c34787b6a573c3c2aba0361fc3fcc0972
mikerainey <bionic.oldsmobile@gmail.com>
2020-09-25 17:51:57 -0400

    introducing flexiblility to pick between different schedulers at grub config

--- src/rt/tpal/benchmark.cpp
+++ src/rt/tpal/benchmark.cpp
@@ -29,7 +29,16 @@ namespace tpalrts {
 
 uint64_t cpu_freq_khz = 0;
 
-char* sched_configurations [] = {
+using scheduler_configuration_type = enum scheduler_configurations_enum {
+  scheduler_configuration_serial,
+  scheduler_configuration_software_polling,
+  scheduler_configuration_interrupt_ping_thread,
+  scheduler_configuration_nopromote_interrupt_ping_thread,
+  scheduler_configuration_manual,
+  nb_scheduler_configurations
+};
+
+char* scheduler_configurations [] = {
   "serial",
   "software_polling",
   "interrupt_ping_thread",
@@ -37,7 +46,9 @@ char* sched_configurations [] = {
   "manual",
 };
 
-char* sched_configuration = "<bogus>";
+char* scheduler_configuration_name = "<bogus>";
+
+scheduler_configuration_type scheduler_configuration = nb_scheduler_configurations;
 
 auto find_string(char* s, char** ss, std::size_t n) -> std::size_t {
   std::size_t i = 0;
@@ -69,8 +80,28 @@ using promotable_function_type = std::function<void(promotable*)>;
  
 using benchmark_type = struct benchmark_struct {
   promotable_function_type pre;
-  promotable_function_type body;
+  std::vector<promotable_function_type> bodies;
   promotable_function_type post;
+};
+
+std::vector<benchmark_type> benchmarks;
+
+template <typename Pre,
+	  typename Body_serial,
+	  typename Body_interrupt,
+	  typename Post>
+auto add_benchmark(const Pre& pre,
+		   const Body_serial& body_serial,
+		   const Body_interrupt& body_interrupt,
+		   const Post& post) {
+  std::vector<promotable_function_type> bodies(nb_scheduler_configurations, body_interrupt);
+  bodies[scheduler_configuration_serial] = promotable_function_type(body_serial);
+  bodies[scheduler_configuration_interrupt_ping_thread] = promotable_function_type(body_interrupt);
+  benchmark_type b = {
+    .pre = promotable_function_type(pre),
+    .bodies = bodies,
+    .post = promotable_function_type(post) };
+  benchmarks.push_back(b);
 };
 
 template <typename Scheduler, typename Worker, typename Interrupt>
@@ -89,19 +120,21 @@ void launch(std::size_t nb_workers,
   fibers.push_back(f_cont);
   fibers.push_back(f_term);
   fiber<Scheduler>::add_edge(f_cont, f_term);
+  aprintf("==========\n");
   for (auto b : benchmarks) {
     auto f_pre = new fiber<Scheduler>([=] (promotable* p) {
       b.pre(p);
       stats::start_collecting();
       start_cycle = mcsl::cycles::now();
     });
-    auto f_body = new fiber<Scheduler>(b.body);
+    auto body = b.bodies[scheduler_configuration];
+    auto f_body = new fiber<Scheduler>(body);
     auto f_post = new fiber<Scheduler>([=] (promotable* p) {
       elapsed_cycles = mcsl::cycles::since(start_cycle);
       {
 	print_header();
 	aprintf("nb_cpus %d\n", nb_cpus);
-	aprintf("scheduler_configuration %s\n", sched_configuration);
+	aprintf("scheduler_configuration %s\n", scheduler_configuration_name);
 	aprintf("---\n");
 	aprintf("proc %lu\n", nb_workers);
 	aprintf("kappa_usec %lu\n", kappa_usec);
@@ -146,39 +179,55 @@ void benchmark_init(int argc, char** argv) {
     } else if (strcmp(argv[i], "scheduler_configuration") == 0) {
       if ((i + 1) < argc) {
 	auto s = argv[i + 1];
-	auto n = sizeof(sched_configurations);
-	auto j = find_string(s, sched_configurations, n);
+	auto n = sizeof(scheduler_configurations);
+	assert(nb_scheduler_configurations == n);
+	auto j = find_string(s, scheduler_configurations, n);
 	if (j < n) {
-	  sched_configuration = sched_configurations[j];
+	  scheduler_configuration = (scheduler_configuration_type)j;
+	  scheduler_configuration_name = scheduler_configurations[j];
 	}
       }      
     }
   }
-  using microbench_scheduler_type = mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, ping_thread_worker, ping_thread_interrupt>;
-  std::vector<benchmark_type> benchmarks;
-   benchmark_type b1 = {
-     .pre = promotable_function_type([] (promotable* p) {
-       print_header = [] {
-	 print_prog("incr_array");
-	 aprintf("n %lu\n", incr_array::nb_items);
-       };
-       incr_array::bench_pre(p);
-     }),
-     .body = promotable_function_type(incr_array::bench_body_interrupt),
-     .post = promotable_function_type(incr_array::bench_post) };
-   benchmarks.push_back(b1);
-   benchmark_type b2 = {
-     .pre = promotable_function_type([] (promotable* p) {
-         print_header = [=] {
-	   print_prog("plus_reduce_array");
-	   aprintf("n %lu\n", plus_reduce_array::nb_items);
-	 };
-	 plus_reduce_array::bench_pre(p);
-     }),
-     .body = promotable_function_type(plus_reduce_array::bench_body_interrupt),
-     .post = promotable_function_type(plus_reduce_array::bench_post) }; 
-     benchmarks.push_back(b2);
-   launch<microbench_scheduler_type, ping_thread_worker, ping_thread_interrupt>(nb_workers, benchmarks);
+  add_benchmark([] (promotable* p) {
+    print_header = [] {
+      print_prog("incr_array");
+      aprintf("n %lu\n", incr_array::nb_items);
+    };
+    incr_array::bench_pre(p);
+  },
+    incr_array::bench_body_serial,
+    incr_array::bench_body_interrupt,
+    incr_array::bench_post);
+  add_benchmark([] (promotable* p) {
+    print_header = [=] {
+      print_prog("plus_reduce_array");
+      aprintf("n %lu\n", plus_reduce_array::nb_items);
+    };
+    plus_reduce_array::bench_pre(p);
+  },
+    plus_reduce_array::bench_body_serial,
+    plus_reduce_array::bench_body_interrupt,
+    plus_reduce_array::bench_post);
+  switch (scheduler_configuration) {
+  case scheduler_configuration_serial: {
+    using microbench_scheduler_type =
+      mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, tpal_worker>;
+    launch<microbench_scheduler_type, tpal_worker, mcsl::minimal_interrupt>(nb_workers, benchmarks);
+    break;
+  }
+  case scheduler_configuration_interrupt_ping_thread: {
+    using microbench_scheduler_type =
+      mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic,
+			      ping_thread_worker, ping_thread_interrupt>;
+    launch<microbench_scheduler_type, ping_thread_worker, ping_thread_interrupt>(nb_workers, benchmarks);
+    break;
+  }
+  default: {
+    aprintf("bogus choice for scheduler\n");
+    return;
+  }
+  }
 }
 
 }
