5502240084777e5278fa78930e4cad0cdc05b744
mikerainey <bionic.oldsmobile@gmail.com>
2020-09-29 11:54:19 -0400

    edit

--- tinker.sh
+++ tinker.sh
@@ -3,5 +3,5 @@
 nix-build --argstr nautilusConfig configs/tinker-config
 # --arg sources 'import ./default-sources.nix'
 
-scp -i ~/.ssh/id_rsa_mac result/nautilus.bin root@tinker-2.cs.iit.edu:/boot/nautilus.bin
-scp -i ~/.ssh/id_rsa_mac result/nautilus.syms root@tinker-2.cs.iit.edu:/boot/nautilus.syms
+scp -i ~/.ssh/id_rsa_mac ./result/nautilus.bin root@tinker-2.cs.iit.edu:/boot/nautilus.bin
+scp -i ~/.ssh/id_rsa_mac ./result/nautilus.syms root@tinker-2.cs.iit.edu:/boot/nautilus.syms
